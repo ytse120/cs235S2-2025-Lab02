bc3d5f6ad5e1bfb7c68e458523ce99db0e14a9fd
Sean Tseng <ytse120@aucklanduni.ac.nz>
2025-08-05 12:09:11 +1200

    Added string formating method for Recipe class objects

--- domainModel/Recipe.py
+++ domainModel/Recipe.py
@@ -8,4 +8,7 @@ class Recipe:
         self._instructions = []
         self._cook_time = "TBD"
         self._category = []
+        self._createdDate = "TBD"
 
+    def __str__(self):
+        return "<Recipe " + self._name + " with id: " + str(self.__id) + " was created by " + self._author + " on " + self._createdDate + ">"
