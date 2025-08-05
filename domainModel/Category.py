426c9417fb18e6f514c1fab443bb494345bc3924
Sean Tseng <ytse120@aucklanduni.ac.nz>
2025-08-05 12:11:57 +1200

    Added string formatting method for Category class object

--- domainModel/Category.py
+++ domainModel/Category.py
@@ -1,6 +1,9 @@
 class Categoru:
-    def __init__(self, name, id):
+    def __init__(self, name):
         self.__id = "TBD"
         self.__name = name
         self.__recipes = []
+
+    def __str__(self):
+        return "<Category " + str(self.__id) + ": " + self.__name + ">"
 
