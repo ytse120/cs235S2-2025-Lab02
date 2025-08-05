49fb3a31fcff8b89f5d22b904ec52c96907101c3
Sean Tseng <ytse120@aucklanduni.ac.nz>
2025-08-05 13:05:33 +1200

    Added properties and essential methods to Recipe and Category class objects

--- domainModel/Category.py
+++ domainModel/Category.py
@@ -1,9 +1,23 @@
-class Categoru:
-    def __init__(self, name):
-        self.__id = "TBD"
+class Category:
+    def __init__(self, id, name):
+        self.__id = id
         self.__name = name
         self.__recipes = []
 
     def __str__(self):
         return "<Category " + str(self.__id) + ": " + self.__name + ">"
 
+    def print_recipe(self):
+        print("Category " + self.__name + " has the following associated recipes: " + str(self.__recipes))
+
+    def add_recipe(self, recipe):
+        if recipe not in self.__recipes:
+            self.__recipes.append(recipe)
+            return True
+        return False
+
+    def remove_recipe(self, recipe):
+        if recipe in self.__recipes:
+            self.__recipes.remove(recipe)
+            return True
+        return False
--- domainModel/Recipe.py
+++ domainModel/Recipe.py
@@ -1,14 +1,45 @@
+import datetime
+
 class Recipe:
-    def __init__(self, name, author):
-        self.__id = "TBD"
+    def __init__(self, id, name, author, date=datetime.datetime.now().replace(microsecond=0), ingredients=[]):
+        self.__id = id
         self._name = name
         self._author = author
-        self._ingredients = []
+        self._ingredients = ingredients
         self._description = ""
         self._instructions = []
         self._cook_time = "TBD"
         self._category = []
-        self._createdDate = "TBD"
+        self._createdDate = date
 
     def __str__(self):
-        return "<Recipe " + self._name + " with id: " + str(self.__id) + " was created by " + self._author + " on " + self._createdDate + ">"
+        return "<Recipe " + self._name + " with id: " + str(self.__id) + " was created by " + self._author + " on " + str(self._createdDate) + ">"
+
+    def add_description(self, description):
+        self._description = description
+
+    def add_category(self, category):
+        if category not in self._category:
+            self._category.append(category)
+            return True
+        return False
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def ingredients(self):
+        return self._ingredients
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def createdDate(self):
+        return self._createdDate
